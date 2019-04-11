9f0c4d2afbb21b1e406a3818223be6b6a9fbacdb
v-zhangkaicheng <ys@123456>
2019-04-11 16:26:25 +0800

    no msg

--- provider/src/main/java/lon/test/provider/services/impl/DemoDubboServiceImpl.java
+++ provider/src/main/java/lon/test/provider/services/impl/DemoDubboServiceImpl.java
@@ -3,21 +3,14 @@ package lon.test.provider.services.impl;
 import lon.test.provider.bean.TestBean;
 import lon.test.provider.services.DemoDubboService;
 import org.apache.dubbo.config.annotation.Service;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Service(version = "1.0")
 @Component
 public class DemoDubboServiceImpl implements DemoDubboService {
 
-    @Value("${server.port}")
-    String port;
-
-    @Value("${spring.application.name}")
-    String appName;
-
     @Override
     public String testDubboApiO(TestBean testBean) {
-        return "port:" + port + ";appName:" + appName + ";param:" + testBean.toString() + ";";
+        return "param:" + testBean.toString() + ";";
     }
 }
